b41a9a944f1dca9cc2857bdce8bd4cfcc37aae3f
Serrones <fabioserrones@gmail.com>
2018-05-06 13:34:05 -0300

    Mongodb added to store logs -- bugs fixed

--- app/mongoland.py
+++ app/mongoland.py
@@ -6,7 +6,7 @@ class MongoLord:
         self.el_mongo = mongo.db.microblog_mdb_log
 
     def mongo_logging(self, request_method, request_endpoint,
-                      request_url, time, request_args):
+                      request_url, time):
         log_info = {"method": request_method, "endpoint": request_endpoint,
-                    "url": request_url, "data": time, "args": request_args}
+                    "url": request_url, "data": time}
         self.el_mongo.insert(log_info)
--- app/routes.py
+++ app/routes.py
@@ -27,16 +27,14 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    log = MongoLord()
+    time = datetime.utcnow()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
                                     form.username.data, form.remember_me.data))
-        log = MongoLord()
-        time = datetime.utcnow()
         log.mongo_logging(request.method, request.endpoint,
                           request.url, time)
         return redirect(url_for('index'))
-    log = MongoLord()
-    time = datetime.utcnow()
     log.mongo_logging(request.method, request.endpoint,
                       request.url, time)
     return render_template('login.html', title='Sign In', form=form)
